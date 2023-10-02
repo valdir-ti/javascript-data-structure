7e5f94fd5c385392cab2a06f278dcbbbbcccc042
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-10-02 17:14:38 -0300

    refact: move fibonacci to a module exports

---
+++ src/fibonacci.js
@@ -0,0 +1,14 @@
+// Fibonacci Sequence
+
+module.exports = function fibonacci(n) {
+
+    let fib = [0,1]
+
+    for(let i = 2; i < n; i++) {
+        fib[i] = fib[i-1] + fib[i-2]
+    }
+
+    return fib
+}
+
+// Big-O = O(n)
--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
-// Fibonacci Sequence
-
-function fibonacci(n) {
-
-    let fib = [0,1]
-
-    for(let i = 2; i < n; i++) {
-        fib[i] = fib[i-1] + fib[i-2]
-    }
-
-    return fib
-}
+const fibonacci = require('./fibonacci')
 
 console.log(fibonacci(2))
 console.log(fibonacci(3))
 console.log(fibonacci(7))
-
-// Big-O = O(n)
