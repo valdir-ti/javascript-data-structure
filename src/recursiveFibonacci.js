1a2108f0a4463ebaf4f2eb4f02b40bada70930ef
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2023-10-05 09:45:03 -0300

    refact: determine complexity

--- src/recursiveFibonacci.js
+++ src/recursiveFibonacci.js
@@ -1,6 +1,11 @@
+// Recursive Fibonacci
+
 module.exports = function recursiveFibonacci (n) {
 
     if( n < 2) return n
 
     return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
 }
+
+// Big-O = O(n) - Iterative
+// O(2^n) - Recursive is not a good aproach to the fibonacci algorithm
